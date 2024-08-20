a51e81509eb1eaa77bb027d263173ed10b055a86
saurabhmohite8386 <saurabhmohite8386@gmail.com>
2024-08-20 11:44:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 const http = require('http');
 const socketio = require('socket.io');
-
-// Create an HTTP server and bind Socket.io to it
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Socket.io connection
-io.on("connection", function(socket) {
-    console.log("New connection:", socket.id);
-
-    // Handle 'send-location' event
-    socket.on("send-location", function(data) {
-        io.emit("receive-location", { id: socket.id, ...data });
+io.on("connection" , function(socket){
+    socket.on("send-location", function(data){
+        io.emit("recieve-location" ,{ id: socket.id, ...data});
     });
 
-    // Handle 'disconnect' event
-    socket.on("disconnect", function() {
-        io.emit("user-disconnected", socket.id);
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id)
     });
+    console.log("connected");
 });
 
-// Set the view engine to EJS
 app.set("view engine", "ejs");
-
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define the main route
-app.get("/", (req, res) => {
-    res.render("index");
-});
+app.get("/",(req,res) =>
+     res.render("index")
+);
 
-// Listen on the specified port (PORT) or default to 3000
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(3000); 
